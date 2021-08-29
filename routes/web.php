f3ee29be70af245322765905de3efed96abb59dd
jjnguirazej <jjnguirazej@gmail.com>
2021-08-30 00:54:41 +0200

    Authentication instalation... with jetStream
    add Category

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//category controller
+Route::get('/category/all', [\App\Http\Controllers\CategoryController::class,'index'])->name('all.category');
+Route::post('/category/add', [\App\Http\Controllers\CategoryController::class,'store'])->name('store.category');
+
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+   $users = \App\Models\User::all();
+    return view('dashboard',compact( 'users'));
+})->name('dashboard');
